e346e6c8711facd56d9f478991ec2c5b7b49a198
Christine Ling <christine.ling@wustl.edu>
2025-11-10 21:59:28 -0600

    Square now displays an "X" when clicked

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,9 @@ function Square() {
   const [value, setValue] = useState(null);
 
   function handleClick() {
-    console.log('clicked!');
+    setValue('X');
   }
+  
   return (
     <button 
       className="square" 
