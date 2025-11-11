b0022976fe02543bbd9190084bedb2fcec79ac88
Christine Ling <christine.ling@wustl.edu>
2025-11-10 22:10:40 -0600

    Pass function called handleClick to the onSquareClick prop of the first Square component

--- src/App.jsx
+++ src/App.jsx
@@ -10,10 +10,10 @@ function Square({ value, onSquareClick }) {
 
 export default function Board() {
   const [squares, setSquares] = useState(Array(9).fill(null));
-   return (
+  return (
     <>
       <div className="board-row">
-        <Square value={squares[0]} />
+        <Square value={squares[0]} onSquareClick={handleClick} />
         <Square value={squares[1]} />
         <Square value={squares[2]} />
       </div>
